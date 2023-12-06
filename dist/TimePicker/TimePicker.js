f11a4426b1eddab677933c2f7c18d2346e590a55
idolieb26 <idolieb26@gmail.com>
2023-12-06 13:51:54 +0200

    fix: test version for debug

--- dist/TimePicker/TimePicker.js
+++ dist/TimePicker/TimePicker.js
@@ -1,88 +1,89 @@
-import { jsx as m } from "react/jsx-runtime";
-import { useRef as v, useState as R } from "react";
-import { Cross as T, Clock as w } from "@strapi/icons";
+import { jsx as u } from "react/jsx-runtime";
+import { useRef as v, useState as H } from "react";
+import { Cross as R, Clock as T } from "@strapi/icons";
 import I from "styled-components";
 import "../Field/Field.js";
 import "../Field/FieldLabel.js";
 import "../Field/FieldInput.js";
 import "../Field/FieldContext.js";
 import "../Typography/Typography.js";
-import { FieldAction as y } from "../Field/FieldAction.js";
-import { Flex as A } from "../Flex/Flex.js";
-import { useId as H } from "../hooks/useId.js";
+import { FieldAction as w } from "../Field/FieldAction.js";
+import { Flex as y } from "../Flex/Flex.js";
+import { useId as A } from "../hooks/useId.js";
 import S from "./InputMask/index.js";
 import { TextInput as V } from "../TextInput/TextInput.js";
-const F = (r, d = 1) => {
-  const o = [];
-  let e = 0;
-  for (let t = 0; t < 24; t++)
-    for (e = 0; e < 60; )
-      o.push(`${t < 10 ? `0${t}` : t}:${e < 10 ? `0${e}` : e}`), e += d;
-  const [n, u] = r?.split(":") ?? [];
-  let i = o.reduce((t, a) => {
-    const [s] = a.split(":");
-    return Math.abs(s - n) < Math.abs(t - n) ? s : t;
-  }, o[0].split(":")[0]);
-  const $ = o.reduce((t, a) => {
-    const s = a.split(":")[1];
-    return Math.abs(s - u) < Math.abs(t - u) ? s : t;
-  }, o[0].split(":")[1]), l = parseInt(i);
-  let c = "am";
-  if (l > 11) {
-    c = l === 24 ? "am" : "pm";
-    const t = l !== 12 ? l - 12 : 12;
-    t < 10 ? i = `0${t}` : i = t.toString();
-  }
-  return `${i}:${$}${c}`;
+const F = (s, d = 1) => {
+  const i = [];
+  let t = 0;
+  for (let e = 0; e < 24; e++)
+    for (t = 0; t < 60; )
+      i.push(`${e < 10 ? `0${e}` : e}:${t < 10 ? `0${t}` : t}`), t += d;
+  const [l, c] = s?.split(":") ?? [];
+  let r = i.reduce((e, m) => {
+    const [n] = m.split(":");
+    return Math.abs(n - l) < Math.abs(e - l) ? n : e;
+  }, i[0].split(":")[0]);
+  const g = i.reduce((e, m) => {
+    const n = m.split(":")[1];
+    return Math.abs(n - c) < Math.abs(e - c) ? n : e;
+  }, i[0].split(":")[1]), o = parseInt(r);
+  let a = "am";
+  if (console.log("intHour:", o), o > 11) {
+    a = o === 24 ? "am" : "pm";
+    const e = o !== 12 ? o - 12 : 12;
+    console.log("hourNumber:", e), e < 10 ? r = e === 0 ? "12" : `0${e}` : r = e.toString();
+  } else
+    o === 0 ? (console.log("else if :", o), a = "am", r = "12") : console.log("else:", o);
+  return `${r}:${g}${a}`;
 }, Y = ({
-  id: r,
+  id: s,
   value: d,
   step: C = 15,
-  clearLabel: o,
-  disabled: e = !1,
-  onClear: n,
-  onChange: u,
-  label: i = "",
-  ...$
+  clearLabel: i,
+  disabled: t = !1,
+  onClear: l,
+  onChange: c,
+  label: r = "",
+  ...g
 }) => {
-  const l = H(r), c = v(null), [t, a] = R(F(d)), s = () => {
-    n && (a("00:00am"), n(), c.current.inputWrapperRef.current.focus());
+  const o = A(s), a = v(null), [e, m] = H(F(d)), n = () => {
+    l && (m("00:00am"), l(), a.current.inputWrapperRef.current.focus());
   };
-  return m(S, {
+  return u(S, {
     mask: [/[0-2]/, /[0-3]/, ":", /[0-5]/, /[0-9]/, /(a|p)/, /[m]/],
-    value: t,
+    value: e,
     onChange: (k) => {
       const M = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9](a|p)m$/, p = k.target.value;
-      if (u && M.test(p)) {
-        const b = !p.includes("am"), g = p.replace("am", "").replace("pm", ""), x = parseInt(g.split(":")[1]);
-        let f = g.split(":")[0];
+      if (c && M.test(p)) {
+        const $ = !p.includes("am"), b = p.replace("am", "").replace("pm", ""), x = parseInt(b.split(":")[1]);
+        let f = b.split(":")[0];
         const h = parseInt(f);
-        (b && h !== 12 || !b && h === 12) && (f = h + 12), u(`${f}:${x}`);
+        ($ && h !== 12 || !$ && h === 12) && (f = h + 12), c(`${f}:${x}`);
       }
-      a(p);
+      m(p);
     },
-    disabled: e,
-    children: m(V, {
-      id: l,
-      ref: c,
-      label: i,
-      startAction: m(P, {
-        children: m(w, {})
+    disabled: t,
+    children: u(V, {
+      id: o,
+      ref: a,
+      label: r,
+      startAction: u(N, {
+        children: u(T, {})
       }),
-      endAction: n ? m(y, {
+      endAction: l ? u(w, {
         label: "close",
-        onClick: s,
-        "aria-disabled": e || void 0,
-        children: m(W, {})
+        onClick: n,
+        "aria-disabled": t || void 0,
+        children: u(P, {})
       }) : void 0,
       "aria-autocomplete": "none",
       "aria-label": "close",
       type: "text",
-      disabled: e,
-      ...$
+      disabled: t,
+      ...g
     })
   });
-}, P = I(A)`
+}, N = I(y)`
   & > svg {
     height: 1rem;
     width: 1rem;
@@ -90,17 +91,17 @@ const F = (r, d = 1) => {
 
   & > svg path {
     fill: ${({
-  theme: r
-}) => r.colors.neutral500};
+  theme: s
+}) => s.colors.neutral500};
   }
-`, W = I(T)`
+`, P = I(R)`
   height: ${11 / 16}rem;
   width: ${11 / 16}rem;
 
   path {
     fill: ${({
-  theme: r
-}) => r.colors.neutral600};
+  theme: s
+}) => s.colors.neutral600};
   }
 `;
 export {
